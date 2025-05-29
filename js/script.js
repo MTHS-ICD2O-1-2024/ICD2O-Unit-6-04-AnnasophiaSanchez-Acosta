08f5ff3f29d0098fc615da0153889350df3a8d33
AnnasophiaSanchez-Acosta <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-05-29 02:12:45 +0000

    space

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall updatedby Anna S.
+// Created by: Mr. Coxall updated by Anna S.
 // Created on: May 2025
 // This file contains the JS functions for index.html
 
